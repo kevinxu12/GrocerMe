55c95cde9c3cebd60796883b1ad85a1a936f9813
Kevin Xu <xukevinwork@gmail.com>
2022-01-25 13:57:12 -0500

    [kexu] Adding caching of dependencies to github action to speed up
    In the future, we might need to switch caching away from the `node_modules` folders back to the `~/.npm' folder.
    The `node_modules` caching is a trick we use, if we assume our `package-lock.json` file won't change super frequently.
    If there's a change in the package lock, we won't revert back to a default cache, we'll make a new cache.

--- server/src/index.ts
+++ server/src/index.ts
@@ -7,7 +7,6 @@ import app from './app';
 import express from 'express';
 
 app.use(express.static(path.resolve(__dirname, '../../client/build')));
-
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, '../../client/build', 'index.html'));
 });
